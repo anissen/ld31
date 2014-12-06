4aabb4682e0db76dd17104a057352042df3fecbf
Anders Nissen <andnis@gmail.com>
2014-12-06 18:35:59 +0100

    Clean up

--- src/Main.hx
+++ src/Main.hx
@@ -11,10 +11,9 @@ import luxe.ParcelProgress;
 class Main extends luxe.Game {
     var level :Level;
     public static var words :Array<String>;
-    var wordGuessText :Text;
 
     override function ready() {
-        Luxe.renderer.clear_color.rgb(0xfdfffc);
+        // Luxe.renderer.clear_color.rgb(0xfdfffc);
 
         var json_asset = Luxe.loadJSON("assets/parcel.json");
 
@@ -28,51 +27,15 @@ class Main extends luxe.Game {
         });
 
         preload.load();
-
-    } //ready
+    }
 
     function assets_loaded(_) {
-        // var wordlist = Luxe.resources.find_text('assets/wordlists/en.txt');
-        // trace('text length: ' + wordlist.text.length);
-
         var wordList = Luxe.resources.find_text('assets/wordlists/en.txt');
         words = wordList.text.split("\n");
 
-        wordGuessText = new Text({
-            // no_scene: true,
-            text: "",
-            pos: new Vector(Luxe.screen.w / 2, Luxe.screen.h - 100),
-            color: new Color().rgb(0xffffff),
-            point_size: 46,
-            align: center, 
-            align_vertical: center
-        });
+        luxe.tween.Actuate.defaultEase = luxe.tween.easing.Elastic.easeOut;
 
         level = new Level();
-        level.events.listen('spelling_word', function(data: { word :String, correct :Bool, alreadyUsed :Bool }) {
-            wordGuessText.scale.set_xy(1, 1);
-
-            wordGuessText.text = data.word;
-            wordGuessText.color.set(0, 0, 0);
-            var color :Dynamic = (data.correct ? (data.alreadyUsed ? { r: 0, g: 0, b: 0.7 } : { r: 0, g: 0.7, b: 0 } ) : { r: 0.7, g: 0, b: 0 });
-            wordGuessText.color
-                .tween(0.2, color)
-                .ease(luxe.tween.easing.Quad.easeInOut);
-        });
-
-        level.events.listen('guessed_word', function(data: { word :String, correct :Bool, alreadyUsed :Bool }) {
-            luxe.tween.Actuate
-                .tween(wordGuessText.scale, 0.3, { x: 0, y: 0 })
-                .ease(luxe.tween.easing.Elastic.easeInOut);
-        });
     }
-
-    override function onkeyup(e :KeyEvent) {
-        switch (e.keycode) {
-            case Key.key_f: app.app.window.fullscreen = !app.app.window.fullscreen;
-            case Key.key_r: level.reset();
-            case Key.escape: Luxe.shutdown();
-        }
-    } //onkeyup
 
 } //Main
