dd616c04e16e16631899cbaaf053838bcf5fa004
Anders Nissen <andnis@gmail.com>
2014-12-22 23:16:41 +0100

    Refactoring, menu screen

--- src/Main.hx
+++ src/Main.hx
@@ -1,11 +1,6 @@
 
 import entities.Level;
-import luxe.Input;
 import luxe.States;
-import luxe.Text;
-import luxe.Vector;
-import luxe.Color;
-
 import luxe.Parcel;
 import luxe.ParcelProgress;
 import states.*;
@@ -15,12 +10,8 @@ class Main extends luxe.Game {
     public static var words :Array<String>;
     public static var states :States;
     var step :Int = 0;
-    var titleText :Text;
-    var introText :Text;
 
     override function ready() {
-        // Luxe.renderer.clear_color = new luxe.ColorHSV(0, 1, 0);
-
         var json_asset = Luxe.loadJSON("assets/parcel.json");
 
         var preload = new Parcel();
@@ -46,121 +37,8 @@ class Main extends luxe.Game {
         states.add(new MenuScreenState());
         states.add(new PlayScreenState());
 
-        states.set('TitleScreenState');
-
-        /*
-        var unique_shader = Luxe.renderer.shaders.bitmapfont.shader.clone();
-        unique_shader.set_float('thickness', 1);
-        unique_shader.set_float('smoothness', 0.8);
-        unique_shader.set_float('outline', 0.75);
-        unique_shader.set_vector4('outline_color', new Vector(0.0, 0.0, 0.0, 1.0));
-
-        titleText = new Text({
-            pos: Vector.Add(Luxe.screen.mid, new Vector(0, -100)),
-            text: 'Train of Thought',
-            font: Luxe.resources.find_font("rail"),
-            shader: unique_shader,
-            color: new ColorHSV(0, 0, 1),
-            align: center,
-            align_vertical: center,
-            point_size: 64
-        });
-
-        introText = new Text({
-            pos: Vector.Add(Luxe.screen.mid, new Vector(0, 100)),
-            text: 'Press Enter for instructions',
-            font: Luxe.resources.find_font("rail"),
-            shader: unique_shader,
-            color: new ColorHSV(0, 0, 0.6),
-            align: center,
-            align_vertical: center,
-            point_size: 42
-        });
-        */
+        // states.set('TitleScreenState');
+        states.set('MenuScreenState');
     }
-
-    /*
-    override function onkeyup(e :KeyEvent) {
-        if (level != null) return;
-        if (e.keycode != Key.enter) return;
-
-        step++;
-        var unique_shader = Luxe.renderer.shaders.bitmapfont.shader.clone(); // .font.shader.clone();
-        unique_shader.set_float('thickness', 1);
-        unique_shader.set_float('smoothness', 0.8);
-        unique_shader.set_float('outline', 0.75);
-        unique_shader.set_vector4('outline_color', new Vector(0.0, 0.0, 0.0, 1.0));
-        if (step == 1) {
-            titleText.destroy();
-            introText.destroy();
-            titleText = new Text({
-                pos: Vector.Add(Luxe.screen.mid, new Vector(0, -200)),
-                text: 'Instructions',
-                font: Luxe.resources.find_font("rail"),
-                shader: unique_shader,
-                color: new ColorHSV(200, 0, 1),
-                align: center,
-                align_vertical: center,
-                point_size: 64
-            });
-
-            introText = new Text({
-                pos: Vector.Add(Luxe.screen.mid, new Vector(-80, -120)),
-                text: '
-                Type words to create a railway
-                track to the station.
-
-                But hurry before the train departs!
-
-                - - -
-
-                Press Enter for controls',
-                font: Luxe.resources.find_font("rail"),
-                shader: unique_shader,
-                color: new ColorHSV(0, 0, 0.6),
-                align: center,
-                align_vertical: top,
-                point_size: 42
-            });
-        } else if (step == 2) {
-            titleText.destroy();
-            introText.destroy();
-            titleText = new Text({
-                pos: Vector.Add(Luxe.screen.mid, new Vector(0, -200)),
-                text: 'Controls',
-                font: Luxe.resources.find_font("rail"),
-                shader: unique_shader,
-                color: new ColorHSV(100, 0, 1),
-                align: center,
-                align_vertical: center,
-                point_size: 64
-            });
-
-            introText = new Text({
-                pos: Vector.Add(Luxe.screen.mid, new Vector(-90, -150)),
-                text: '
-                Arrow keys - Move the cursor
-                    A to Z - Type letters
-                 Backspace - Erase last letter
-                     Enter - Submit word
-                    Escape - Scrap word
-
-                - - -
-
-                Press Enter to begin',
-                font: Luxe.resources.find_font("rail"),
-                shader: unique_shader,
-                color: new ColorHSV(0, 0, 0.6),
-                align: center,
-                align_vertical: top,
-                point_size: 42
-            });
-        } else {
-            titleText.destroy();
-            introText.destroy();
-            level = new Level();
-        }
-    }
-    */
 
 } //Main
--- src/states/MenuScreenState.hx
+++ src/states/MenuScreenState.hx
@@ -2,14 +2,84 @@
 package states;
 
 import luxe.Color;
-import luxe.Input.KeyEvent;
-import luxe.Input.Key;
+import luxe.Component;
+import luxe.Entity;
+import luxe.options.TextOptions;
+import luxe.Rectangle;
 import luxe.Scene;
+import luxe.Sprite;
 import luxe.States;
 import luxe.Text;
 import luxe.tween.Actuate;
 import luxe.Vector;
 import luxe.Visual;
+import luxe.Input;
+
+typedef ButtonOptions = {
+
+    > luxe.options.TextOptions,
+
+        /** Callback on click */
+    @:optional var callback :Void -> Void;
+
+}
+
+class Button extends Text {
+    var callback :Void -> Void;
+    var background :Sprite;
+    var height :Float;
+    var width :Float;
+    var mouse_over :Bool = false;
+
+    public function new(_options: ButtonOptions) {
+        var options = _options;
+        if (options.align == null) options.align = TextAlign.center;
+        if (options.align_vertical == null) options.align_vertical = TextAlign.center;
+        if (options.point_size == null) options.point_size = 32;
+        height = 80;
+        width = Luxe.screen.w;
+
+        super(options);
+        callback = _options.callback;
+    }
+
+    override function init() {
+        background = new Sprite({
+            parent: this,
+            texture: Luxe.loadTexture('assets/images/track_straight_rotated.png'),
+            uv: new Rectangle(0, 0, Luxe.screen.w * (128/80), 128),
+            size: new Vector(width, height),
+            origin: new Vector(width / 2, height / 2),
+            color: new Color(255, 255, 255, 0.1)
+        });
+        background.texture.clamp = repeat;
+    }
+
+    override function update(dt :Float) {
+        if (mouse_over) {
+            background.uv.x += 10 * dt;
+        }
+    }
+
+    override function onmousemove(e: MouseEvent) {
+        if (callback == null) return; // button is disabled
+
+        if (!mouse_over && Luxe.utils.geometry.point_in_geometry(e.pos, background.geometry)) {
+            mouse_over = true;
+            background.color.tween(0.5, { a: 0.6 }).ease(luxe.tween.easing.Quad.easeInOut);
+        } else if (mouse_over && !Luxe.utils.geometry.point_in_geometry(e.pos, background.geometry)) {
+            mouse_over = false;
+            background.color.tween(0.5, { a: 0.1 }).ease(luxe.tween.easing.Quad.easeInOut);
+        }
+    }
+
+    override function onmousedown(e: MouseEvent) {
+        if (callback == null) return; // button is disabled
+
+        if (Luxe.utils.geometry.point_in_geometry(e.pos, background.geometry))
+            callback();
+    }
+}
 
 class MenuScreenState extends State {
     var scene :Scene;
@@ -36,15 +106,56 @@ class MenuScreenState extends State {
         });
 
         titleText = new Text({
-            pos: new Vector(Luxe.screen.w / 2, Luxe.screen.mid.y),
-            text: 'This is the menu screen.\nPress Enter',
+            pos: new Vector(Luxe.screen.w / 2, 100),
+            text: 'Train of Thought',
             align: TextAlign.center,
             align_vertical: TextAlign.center,
+            point_size: 46,
             scene: scene,
             parent: background
         });
 
+        var buttonOptions = [
+        {
+            text: 'Easy',
+            description: 'Slow train, letters easier to match',
+            options: {
+
+            }
+        },
+        {
+            text: 'Medium',
+            description: '',
+            options: {
+                
+            }
+        },
+        {
+            text: 'Hard',
+            description: 'Faster train, no help with letters',
+            options: {
+                
+            }
+        }];
+        var yPos = 250;
+        for (opts in buttonOptions) {
+            var button = new Button({
+                text: opts.text, 
+                pos: new Vector(Luxe.screen.w / 2, yPos), 
+                size: new Vector(Luxe.screen.w, 50),
+                scene: scene,
+                callback: play.bind(opts.options)
+            });
+            button.transform.parent = background.transform;
+
+            yPos += 100;
+        }
+
         Actuate.tween(background.pos, 0.5, { x: 0 });
+    }
+
+    function play(options) {
+        Main.states.set('PlayScreenState', options);
     }
 
     override function onleave<T>(_value :T) {
@@ -54,10 +165,5 @@ class MenuScreenState extends State {
             .onComplete(function() {
                 scene.empty();    
             });
-    }
-
-    override function onkeyup(e :KeyEvent) {
-        if (e.keycode != Key.enter) return;
-        Main.states.set('PlayScreenState');
     }
 }
